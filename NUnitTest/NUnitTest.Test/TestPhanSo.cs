d966114481f369eef9c784ee1ed1d12cba437bad
nam <tln9815571@gmail.com>
2019-10-21 13:37:12 +0700

    Add new file Nunittest

---
+++ NUnitTest/NUnitTest.Test/TestPhanSo.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnitTest;
+using NUnit.Framework;
+namespace NUnitTest.Test
+{
+    [TestFixture]
+    public class TestPhanSo
+    {
+        [Test]
+        public void TestHamMacDinh()
+        {
+            PhanSo p=new PhanSo();
+            Assert.AreEqual(0, p.Tu, "Loi o tu so");
+            Assert.AreEqual(1, p.Mau, "Loi o mau so");
+        }
+
+        [Test]
+        public void TestHamDung2ThamSo()
+        {
+            PhanSo p;
+            p = new PhanSo(3, 4);
+            Assert.AreEqual(3, p.Tu, "Loi o tu so");
+            Assert.AreEqual(4,p.Mau,"loi o mau so");
+        }
+
+        [Test]
+        public void TestCong_PhanSoDuong()
+        {
+            PhanSo p1=new PhanSo(1,2);
+            PhanSo p2 = new PhanSo(3, 4);
+            PhanSo p3 = p1.Cong(p2);
+            Assert.AreEqual(10, p3.Tu, "Loi o tu so");
+            Assert.AreEqual(8, p3.Mau, "loi o mau so");
+        }
+        [Test]
+        public void TestCong_PhanSoAm()
+        {
+            PhanSo p1 = new PhanSo(-1, 2);
+            PhanSo p2 = new PhanSo(3, 4);
+            PhanSo p3 = p1.Cong(p2);
+            Assert.AreEqual(2, p3.Tu, "Loi o tu so");
+            Assert.AreEqual(8, p3.Mau, "loi o mau so");
+        }
+        [Test]
+        public void TestTru()
+        {
+            PhanSo p1 = new PhanSo(1, 2);
+            PhanSo p2 = new PhanSo(3, 4);
+            PhanSo p3 = p1.Tru(p2);
+            Assert.AreEqual(-2, p3.Tu, "Loi o tu so");
+            Assert.AreEqual(8, p3.Mau, "loi o mau so");
+        }
+        [Test]
+        public void TestChia()
+        {
+            PhanSo p1 = new PhanSo(1, 2);
+            PhanSo p2 = new PhanSo(3, 4);
+            PhanSo p3 = p1.Chia(p2);
+            Assert.AreEqual(4, p3.Tu, "Loi o tu so");
+            Assert.AreEqual(6, p3.Mau, "loi o mau so");
+        }
+        [Test,ExpectedException(typeof(DivideByZeroException))]
+        public void TestChia0()
+        {
+            PhanSo p1 = new PhanSo(1, 2);
+            PhanSo p2 = new PhanSo(0, 4);
+            PhanSo p3 = p1.Chia(p2);
+        }
+
+        public void TestToString()
+        {
+            PhanSo kq = new PhanSo(3, 4);
+            string s = kq.ToString();
+            Assert.AreEqual("3/4", s, "loi ket qua xuat chuoi");
+        }
+    }
+}
---
+++ NUnitTest/NUnitTest/PhanSo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTest
+{
+    public class PhanSo
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public PhanSo()
+        {
+            tu = 0;
+            mau = 1;
+        }
+        public PhanSo(int tu,int mau)
+        {
+            this.tu = tu;
+            this.mau = mau;
+        }
+        private int tu;
+
+        public int Tu
+        {
+            get { return tu; }
+            set { tu = value; }
+        }
+
+        private int mau;
+
+        public int Mau
+        {
+            get { return mau; }
+            set { mau = value; }
+        }
+
+        public PhanSo Cong(PhanSo p)
+        {
+            PhanSo kq;
+            kq = new PhanSo();
+            kq.tu = tu * p.mau + mau * p.tu;
+            kq.mau = mau * p.mau;
+            return kq;
+        }
+
+        public PhanSo Tru(PhanSo p)
+        {
+            PhanSo kq;
+            kq = new PhanSo();
+            kq.tu = tu * p.mau - mau * p.tu;
+            kq.mau = mau * p.mau;
+            return kq;
+        }
+
+        public PhanSo Chia(PhanSo p)
+        {
+            if (p.tu == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            PhanSo kq;
+            kq = new PhanSo();
+            kq.tu = tu * p.mau;
+            kq.mau = mau * p.tu;
+            return kq;
+        }
+
+        public override string ToString()
+        {
+            return tu + "/" + mau;
+        }
+        
+
+    }
+}
